ace4e8592f4adb2c13feaa5307ecaae36f69715f
Tesfanesh Yisaiase <tesfanesh65tg@gmail.com>
2024-02-29 12:22:26 +0300

    second

--- SpeakGoodBye.js
+++ SpeakGoodBye.js
@@ -4,6 +4,15 @@
 // STEP 6: Wrap the entire contents of SpeakGoodBye.js inside of an IIFE
 // See Lecture 52, part 2
 
+(function (window) {
+  var byeSpeaker = {};
+
+  byeSpeaker.speak = function (name) {
+    console.log("Good Bye " + name);
+  };
+
+  window.byeSpeaker = byeSpeaker;
+})(window);
 
 // STEP 7: Create an object, called 'byeSpeaker' to which you will attach
 // the "speak" method and which you will expose to the global context
